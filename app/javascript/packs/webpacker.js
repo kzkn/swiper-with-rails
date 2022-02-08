09738559f86323e0249a6b1907d3ebea9f6ad41d
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2022-02-08 21:43:35 +0900

    use swiper with webpacker 5

---
+++ app/javascript/packs/webpacker.js
@@ -0,0 +1,8 @@
+import Swiper from 'swiper'
+/* import 'swiper/css' // NOTE: compile error */
+import 'swiper/swiper.min.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('webpacker')
+  new Swiper('.swiper')
+})
