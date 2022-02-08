3abfc0042edbffa757b8e23451cd8c474263b374
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2022-02-08 22:11:06 +0900

    use swiper with webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 const path    = require("path")
 const webpack = require("webpack")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    webpack: "./app/javascript/webpack.js"
   },
   output: {
     filename: "[name].js",
-    sourceMapFilename: "[name].js.map",
+    sourceMapFilename: "[name][ext].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
 }
